ae9311bc219414786abe72b7c8a382bf3fd66015
Xavier <xavierh93@hotmail.com>
2024-07-18 05:25:58 -0400

    finish navbar mostly

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,8 @@
 
 import { useState } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import NavLink from './navLink'
 
 const links = [
   { url: '/', title: 'Home' },
@@ -14,23 +16,40 @@ const Navbar = () => {
   const [open, setOpen] = useState(false)
 
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20">
-      <div className="hidden md:flex gap-4">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 text-xl">
+      {/* LINKS */}
+      <div className="hidden md:flex gap-4 w-1/3">
         {links.map((link) => (
-          <Link href={link.url} key={link.title}>
-            {link.title}
-          </Link>
+          <NavLink link={link} key={link.title} />
         ))}
       </div>
       {/* LOGO */}
-      <div className="md:hidden">
+      <div className="sm:hidden md:hidden lg:flex xl:w-1/3 xl:justify-center">
         <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
           <span className="text-white mr-1 px-1">Xavier</span>
           <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">Elon</span>
         </Link>
       </div>
+      {/* SOCIAL */}
+      <div className="hidden md:flex gap-4 w-1/3 justify-end">
+        <Link href="https://github.com/XavierElon">
+          <Image src="/github.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/linkedin.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/facebook.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/instagram.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/dribbble.png" alt="" width={24} height={24} />
+        </Link>
+      </div>
       {/* RESPONSIVE MENU */}
-      <div className="md:hidden">
+      <div className="md:hidden sm:flex sm:justify-items-end">
         <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={() => setOpen((prev) => !prev)}>
           <div className="w-10 h-1 bg-black rounded"></div>
           <div className="w-10 h-1 bg-black rounded"></div>
