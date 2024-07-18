f8d225db444eef71290124d9d45a3479744d7294
Xavier <xavierh93@hotmail.com>
2024-07-18 06:42:00 -0400

    cool menu transitions

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,94 @@
 'use client'
 
-import { useState } from 'react'
-import Link from 'next/link'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
 import NavLink from './navLink'
+import { motion } from 'framer-motion'
 
 const links = [
   { url: '/', title: 'Home' },
   { url: '/about', title: 'About' },
   { url: '/portfolio', title: 'Portfolio' },
+  { url: 'https://alphaprogramming.io', title: 'Blog' },
   { url: '/contact', title: 'Contact' }
 ]
 
 const Navbar = () => {
   const [open, setOpen] = useState(false)
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768 && open) {
+        setOpen(false)
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [open])
+
+  const topVariants = {
+    closed: {
+      rotate: 0
+    },
+    opened: {
+      rotate: 45,
+      backgroundColor: 'rgb(255,255,255)'
+    }
+  }
+  const centerVariants = {
+    closed: {
+      opacity: 1
+    },
+    opened: {
+      opacity: 0
+    }
+  }
+
+  const bottomVariants = {
+    closed: {
+      rotate: 0
+    },
+    opened: {
+      rotate: -45,
+      backgroundColor: 'rgb(255,255,255)'
+    }
+  }
+
+  const listVariants = {
+    closed: {},
+    opened: {
+      transition: {
+        staggerChildren: 0.2
+      }
+    }
+  }
+
+  const listItemVariants = {
+    closed: ({ isEven }) => ({
+      x: isEven ? '100%' : '-100%',
+      opacity: 0
+    }),
+    opened: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.8,
+        ease: 'easeOut',
+        opacity: {
+          duration: 1,
+          ease: 'easeInOut'
+        }
+      }
+    }
+  }
+
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 text-xl">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
       {/* LINKS */}
       <div className="hidden md:flex gap-4 w-1/3">
         {links.map((link) => (
@@ -24,46 +96,52 @@ const Navbar = () => {
         ))}
       </div>
       {/* LOGO */}
-      <div className="sm:hidden md:hidden lg:flex xl:w-1/3 xl:justify-center">
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
         <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
-          <span className="text-white mr-1 px-1">Xavier</span>
+          <span className="text-white mr-1">Xavier</span>
           <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">Elon</span>
         </Link>
       </div>
       {/* SOCIAL */}
-      <div className="hidden md:flex gap-4 w-1/3 justify-end">
-        <Link href="https://github.com/XavierElon">
+      <div className="hidden md:flex gap-4 w-1/3">
+        <Link href="/">
           <Image src="/github.png" alt="" width={24} height={24} />
         </Link>
         <Link href="/">
-          <Image src="/linkedin.png" alt="" width={24} height={24} />
-        </Link>
-        <Link href="/">
-          <Image src="/facebook.png" alt="" width={24} height={24} />
+          <Image src="/dribbble.png" alt="" width={24} height={24} />
         </Link>
         <Link href="/">
           <Image src="/instagram.png" alt="" width={24} height={24} />
         </Link>
         <Link href="/">
-          <Image src="/dribbble.png" alt="" width={24} height={24} />
+          <Image src="/facebook.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/pinterest.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/linkedin.png" alt="" width={24} height={24} />
         </Link>
       </div>
       {/* RESPONSIVE MENU */}
-      <div className="md:hidden sm:flex sm:justify-items-end">
+      <div className="md:hidden">
+        {/* MENU BUTTON */}
         <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={() => setOpen((prev) => !prev)}>
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
+          <motion.div variants={topVariants} animate={open ? 'opened' : 'closed'} className="w-10 h-1 bg-black rounded origin-left"></motion.div>
+          <motion.div variants={centerVariants} animate={open ? 'opened' : 'closed'} className="w-10 h-1 bg-black rounded"></motion.div>
+          <motion.div variants={bottomVariants} animate={open ? 'opened' : 'closed'} className="w-10 h-1 bg-black rounded origin-left"></motion.div>
         </button>
         {/* MENU LIST */}
         {open && (
-          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
-            {links.map((link) => (
-              <Link href={link.url} key={link.title}>
-                {link.title}
-              </Link>
+          <motion.div variants={listVariants} initial="closed" animate="opened" className="absolute top-0 left-0 w-screen h-screen bg-black bg-opacity-90 text-white flex flex-col items-center justify-center gap-8 text-4xl z-40">
+            {links.map((link, index) => (
+              <motion.div key={link.title} custom={{ isEven: index % 2 === 0 }} variants={listItemVariants} className="overflow-hidden">
+                <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.8, ease: 'easeInOut' }}>
+                  <Link href={link.url}>{link.title}</Link>
+                </motion.div>
+              </motion.div>
             ))}
-          </div>
+          </motion.div>
         )}
       </div>
     </div>
