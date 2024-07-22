fe7d38822bf544a3d663f4c38210dbad55f10972
Xavier <xavierh93@hotmail.com>
2024-07-22 16:27:30 -0400

    navbar styling

--- src/components/dropDown.jsx
+++ src/components/dropDown.jsx
@@ -17,28 +17,34 @@ const DropdownButton = styled.button`
 `
 
 const DropdownContent = styled.div`
-  position: relative;
+  position: absolute;
+  top: 100%;
+  left: 0;
   z-index: 1000;
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 8px 32px;
-  padding: 26px 24px 16px; // Increased top padding
+  padding: 26px 24px 16px;
   border-radius: 12px;
   background: white;
   will-change: transform;
   filter: drop-shadow(0 -5.9px 2.7px hsl(var(--dropdown-shadow-color, 0deg 0% 0%) / 0.025)) drop-shadow(0 -1.2px 6.9px hsl(var(--dropdown-shadow-color, 0deg 0% 0%) / 0.025)) drop-shadow(0 8px 14.2px hsl(var(--dropdown-shadow-color, 0deg 0% 0%) / 0.05)) drop-shadow(0 21.9px 29.2px hsl(var(--dropdown-shadow-color, 0deg 0% 0%) / 0.05)) drop-shadow(0 49px 80px hsl(var(--dropdown-shadow-color, 0deg 0% 0%) / 0.07));
 
+  opacity: ${(props) => (props.isOpen ? 1 : 0)};
+  visibility: ${(props) => (props.isOpen ? 'visible' : 'hidden')};
+  transform: translateY(${(props) => (props.isOpen ? '0' : '-10px')});
+  transition: opacity 0.3s ease, visibility 0.3s ease, transform 0.3s ease;
+
   &::before {
     content: '';
     position: absolute;
     top: -10px;
-    left: 50%;
-    transform: translateX(-50%);
+    left: 20%;
+    transform: translateX(-70%) rotate(45deg);
     width: 20px;
     height: 20px;
     background: white;
-    border-radius: 4px 0;
-    transform: rotate(45deg);
+    border-radius: 10px 0;
     box-shadow: -3px -3px 5px rgba(0, 0, 0, 0.04);
   }
 `
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -96,14 +96,14 @@ const Navbar = () => {
         ))}
       </div>
       {/* LOGO */}
-      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
+      <div className="md:hidden lg:flex xl:w-1/3 justify-center">
         <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
           <span className="text-white mr-1">Xavier</span>
           <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">Elon</span>
         </Link>
       </div>
       {/* SOCIAL */}
-      <div className="hidden md:flex gap-4 w-1/3">
+      <div className="hidden md:flex gap-4 w-1/3 justify-end">
         <Link href="/">
           <Image src="/github.png" alt="" width={24} height={24} />
         </Link>
