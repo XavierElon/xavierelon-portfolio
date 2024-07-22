2e9de60a25b846f73935dd3c0f0b343529c60ff5
Xavier <xavierh93@hotmail.com>
2024-07-22 16:56:19 -0400

    working on page transitions

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -1,12 +1,17 @@
 'use client'
 
 import Navbar from './navbar'
-import { AnimatePresence } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
+import { usePathname } from 'next/navigation'
 
 const TransitionProvider = ({ children }) => {
+  const pathName = usePathname()
+
   return (
-    <AnimatePresence>
-      <div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
+    <AnimatePresence mode="wait">
+      <div key={pathName} className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
+        <motion.div className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40" animate={{ height: '0vh' }} exit={{ height: '140vh' }} transition={{ duration: 0.5, ease: 'easeOut' }} />
+        <motion.div className="h-screen w-screen fixed bg-black rounded-t-[100px] bottom-0 z-30" initial={{ height: '140vh' }} animate={{ height: '0vh', transition: { delay: 0.5 } }} />
         <div className="h-24">
           <Navbar />
         </div>
