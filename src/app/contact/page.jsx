d999df1766c1e44f41b056bf57d38692c91ad7d7
Xavier <xavierh93@hotmail.com>
2024-08-01 21:00:07 -0400

    start contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,13 +1,25 @@
-'use client'
+'use client';
 
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 
 const ContactPage = () => {
   return (
-    <motion.div className="h-full" initial={{ y: '-200vh' }} animate={{ y: '0%' }} transition={{ duration: 1 }}>
-      Contact
+    <motion.div
+      className="h-full"
+      initial={{ y: '-200vh' }}
+      animate={{ y: '0%' }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2">
+          <motion.div>{text.split('')}</motion.div>
+        </div>
+        {/* FORM CONTAINER */}
+        <form className="h-1/2 lg:h-full lg:w-1/2"></form>
+      </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default ContactPage
+export default ContactPage;
